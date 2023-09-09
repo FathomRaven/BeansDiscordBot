8d56bd3858866ce03db2a6f628d33405e0afd870
Fathom <46632358+FathomRaven@users.noreply.github.com>
2023-09-08 20:37:50 -0500

    Commented main file

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-# This example requires the 'members' and 'message_content' privileged intents to function.
+# Bot needs member permissions and message permissions
 
 import discord
 from discord.ext import commands
+
+# Dotenv includes
 import dotenv_vault
 import os
 
+# All commands and cogs
 from cogs.misc_cog import Misc
 from cogs.fun_cog import Fun
 from help_command import Help
@@ -22,6 +25,8 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix=os.getenv("PREFIX"), description=description, intents=intents, help_command=Help())
 
+# Run a check to prevent mass pings
+# TODO: Add in a check for role pings as well
 @bot.check
 async def check(ctx: commands.Context):
     if "@here" in ctx.message.content or "@everyone" in ctx.message.content:
@@ -29,6 +34,7 @@ async def check(ctx: commands.Context):
         return False
     return True
 
+# Ready the bot, and load all cogs
 @bot.event
 async def on_ready():
     await bot.add_cog(Misc(bot))
@@ -36,5 +42,5 @@ async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
 
-
+# Access the token using env
 bot.run(os.getenv("TOKEN"))
